34c11d5e7320969cb7c856745acafef2f40f7974
gauthk6 <gauthk6@gmail.com>
2025-10-17 18:15:46 -0700

    Replace site with new Vite + React portfolio and Pages workflow

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Attempt to unregister any legacy service workers from the old site and clear stale caches
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((registration) => {
+      registration.unregister().catch(() => {})
+    })
+  }).catch(() => {})
+}
+
+if (typeof caches !== 'undefined') {
+  caches.keys().then((cacheNames) => {
+    cacheNames.forEach((cacheName) => {
+      caches.delete(cacheName).catch(() => {})
+    })
+  }).catch(() => {})
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
